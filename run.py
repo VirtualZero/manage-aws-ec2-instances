2c39e360f40b74972cf61210927df142d039b737
Birtchum <birtchum@virtualzero.net>
2019-03-07 18:46:56 -0500

    Added ability to start/stop instances

--- run.py
+++ run.py
@@ -19,13 +19,53 @@ script_directory = os.path.dirname(
 )
 
 
-def write_to_monitoring_log(response, status_message):
-    with open('monitoring.log', 'r+') as monitoring_log:
+def stop_instance():
+    instance_id = input('Enter the AWS EC2 instance ID: ')
+
+    with Halo(
+        text='Stopping Instance...',
+        spinner='dots',
+        text_color='white',
+        color='green'
+    ) as spinner:
+        response = ec2.stop_instances(
+            InstanceIds=[instance_id],
+            DryRun=False
+        )
+
+        print(response)
+
+        if "'stopping'" in str(response) or\
+           "'stopped'" in str(response):
+            unicode_chars = '\n\u2714 '
+            status_message = f'{colored(unicode_chars, "green")}'\
+                f'AWS EC2 instance {instance_id} is '\
+                f'stopped. Meta data about every '\
+                f'start/stop event is located in '\
+                f'{script_directory}/instance_state.log\n'
+
+            check_for_log('instance_state')
+            spinner.stop()
+            write_to_log(
+                'instance_state',
+                response,
+                status_message
+            )
+
+        else:
+            unicode_chars = '\n\u2718 '
+            print(
+                f'{unicode_chars} Something went wrong, try again.'
+            )
+
+
+def write_to_log(log_name, response, status_message):
+    with open(f'{log_name}.log', 'r+') as log:
         log_entry_time = datetime.datetime.now().strftime(
             '%A, %D %I:%M %p'
         )
 
-        monitoring_meta_data = json.dumps(
+        meta_data = json.dumps(
             response,
             indent=4,
             sort_keys=True,
@@ -33,20 +73,65 @@ def write_to_monitoring_log(response, status_message):
         )
 
         log_entry = f'{log_entry_time}\n'\
-            f'{monitoring_meta_data}\n'
+            f'{meta_data}\n'
 
-        log_content = monitoring_log.read()
-        monitoring_log.seek(0, 0)
+        log_content = log.read()
+        log.seek(0, 0)
         updated_log_content = log_entry.rstrip("\r\n") +\
             '\n\n\n' + log_content
 
-        monitoring_log.write(
+        log.write(
             updated_log_content
         )
 
         print(status_message)
 
         return True
+
+
+def start_instance():
+    instance_id = input('Enter the AWS EC2 instance ID: ')
+
+    with Halo(
+        text='Starting Instance...',
+        spinner='dots',
+        text_color='white',
+        color='green'
+    ) as spinner:
+        response = ec2.start_instances(
+            InstanceIds=[instance_id], 
+            DryRun=False
+        )
+
+        if "'pending'" in str(response) or\
+           "'running'" in str(response):
+            unicode_chars = '\n\u2714 '
+            status_message = f'{colored(unicode_chars, "green")}'\
+                f'AWS EC2 instance {instance_id} is '\
+                f'started. Meta data about every '\
+                f'start/stop event is located in '\
+                f'{script_directory}/instance_state.log\n'
+
+            check_for_log('instance_state')
+            spinner.stop()
+            write_to_log(
+                'instance_state',
+                response, 
+                status_message
+            )
+
+        else:
+            unicode_chars = '\n\u2718 '
+            print(
+                f'{unicode_chars} Something went wrong, try again.'
+            )
+
+
+def check_for_log(log_name):
+    if not os.path.isfile(f'{script_directory}/{log_name}.log'):
+        open(f'{log_name}.log', 'a').close()
+
+    return True
 
 
 def enable_monitoring():
@@ -71,12 +156,13 @@ def enable_monitoring():
                 f'monitoring event is located in '\
                 f'{script_directory}/monitoring.log\n'
 
-            if not os.path.isfile(f'{script_directory}/monitoring.log'):
-                open('monitoring.log', 'a').close()
-            
+            check_for_log('monitoring')
             spinner.stop()
-
-            write_to_monitoring_log(response, status_message)
+            write_to_log(
+                'monitoring', 
+                response, 
+                status_message
+            )
 
         else:
             unicode_chars = '\n\u2718 '
@@ -98,8 +184,6 @@ def disable_monitoring():
             InstanceIds=[instance_id]
         )
 
-        spinner.stop()
-
     if "'disabling'" in str(response) or\
        "'disabled'" in str(response):
         unicode_chars = '\n\u2714 '
@@ -109,12 +193,13 @@ def disable_monitoring():
             f'monitoring event is located in '\
             f'{script_directory}/monitoring.log\n'
         
-        if not os.path.isfile(f'{script_directory}/monitoring.log'):
-            open('monitoring.log', 'a').close()
-
+        check_for_log('monitoring')
         spinner.stop()
-
-        write_to_monitoring_log(response, status_message)
+        write_to_log(
+            'monitoring',
+            response, 
+            status_message
+        )
 
     else:
         unicode_chars = '\n\u2718 '
@@ -159,27 +244,39 @@ def parse_cli_arguments():
     parser.add_argument(
         '-i',
         '--info',
-        help='Retrieves and displays information about your AWS EC2 instances.',
+        help='Retrieves and displays information about your AWS EC2 instances',
         action='store_true'
     )
 
     parser.add_argument(
         '-m',
         '--monitor',
-        help='Enables detailed monitoring of an AWS EC2 instance.',
+        help='Enables detailed monitoring of an AWS EC2 instance',
         action='store_true'
     )
 
     parser.add_argument(
         '-u',
         '--unmonitor',
-        help='Disables detailed monitoring of an AWS EC2 instance.',
+        help='Disables detailed monitoring of an AWS EC2 instance',
         action='store_true'
     )
 
-    args = parser.parse_args()
+    parser.add_argument(
+        '-s',
+        '--start',
+        help='Starts an AWS EC2 instance',
+        action='store_true'
+    )
 
-    return args
+    parser.add_argument(
+        '-S',
+        '--stop',
+        help='Stops an AWS EC2 instance',
+        action='store_true'
+    )
+
+    return parser.parse_args()
 
 
 def main():
@@ -193,6 +290,12 @@ def main():
 
     if args.unmonitor:
         disable_monitoring()
+
+    if args.start:
+        start_instance()
+
+    if args.stop:
+        stop_instance()
 
 
 if __name__ == '__main__':
